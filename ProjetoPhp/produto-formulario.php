ea824ac409ec365964626689993c470c95eb6f96
Vinicius Rocha <Vinicius Rocha@DESKTOP-82BUANC>
2019-05-16 20:32:00 -0300

    Inicio da aplicação loja de produtos

---
+++ ProjetoPhp/produto-formulario.php
@@ -0,0 +1,12 @@
+<?php include("cabecalho.php"); ?>
+
+<h1>Formulário de cadastro</h1>
+<form action="adiciona-produto.php">
+    Nome: <input type="text" name="nome" /><br/>
+    Preço: <input type="number" name="preco" /><br/>
+
+    <button type="submit">Cadastrar</button>
+
+</form>
+
+<?php include("rodape.php"); ?>
