de7903071e3650b8236a275a20c0f9103212d50a
Victor Hugo <victorhugocaetanobsb@gmail.com>
2019-05-28 07:50:03 -0300

    Criando Página Sobre.php.

---
+++ ProjetoPhp/sobre.php
@@ -0,0 +1,8 @@
+<?php include("cabecalho.php"); ?>
+
+<h1>Quem somos</h1>
+<h3>Somos uma empresa brasileira do segmento de varejo de produtos tecnológicos fundada em 2019 na cidade de Sobradinho pelos sócios Vinicius Rodrigues e Victor Hugo.</br>
+Temos nossa sede localizada no Shopping de sobradinho e redes de destribuição em Planaltina, Taguatinga e Gama.
+
+</h3>
+<?php include("rodape.php"); ?>
