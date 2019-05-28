c76f52274151a645d177b652cc8e9b13c8e5716e
Victor Hugo <victorhugocaetanobsb@gmail.com>
2019-05-28 08:03:47 -0300

    Realocando API Google Maps a Página Sobre.

--- ProjetoPhp/sobre.php
+++ ProjetoPhp/sobre.php
@@ -4,5 +4,13 @@
 <h3>Somos uma empresa brasileira do segmento de varejo de produtos tecnológicos fundada em 2019 na cidade de Sobradinho pelos sócios Vinicius Rodrigues e Victor Hugo.</br>
 Temos nossa sede localizada no Shopping de sobradinho e redes de destribuição em Planaltina, Taguatinga e Gama.
 
-</h3>
+</h3><br><br><br>
+  <h2>Nossa localização no mapa:</h2><br>
+<iframe
+ width="600"
+ height="450"
+ frameborder="0" style="border:0"
+ src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDyPpLtaDnFwrlqwYh6hFWY1KBjB3m2A04
+ &q=Brasilia+Sobradinho+Sobradinho+shopping" allowfullscreen>
+</iframe>
 <?php include("rodape.php"); ?>
